79a1be149810c1f9ce8cfa23dbe7116341ad4674
Wassim Azaouzi <116125811+wassim-azaouzi@users.noreply.github.com>
2024-02-04 23:42:39 -0600

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
 	public static void main(String[] args) {
-        System.out.println("My name is oumayna , I am 24 years old and I live in chicago Illinois ");
+        System.out.println("My name is wassim , I am 24 years old and I live in chicago Illinois ");
 	}
 }
